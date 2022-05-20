77ada5fea24244cc7baa070e18b0c16515ed08ec
Juan Ignacio <zalbak87@gmail.com>
2022-05-19 22:20:26 -0300

    Clase Video 33 Admin

--- ProyectoWeb/servicios/admin.py
+++ ProyectoWeb/servicios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Servicio
 
 # Register your models here.
+
+class ServicioAdmin(admin.ModelAdmin): #Clse para leer los campos created y updated
+    readonly_fields=('created','updated')
+
+admin.site.register(Servicio,ServicioAdmin)#Registramos en el admin la clase sevisio que es el modelado de la base
