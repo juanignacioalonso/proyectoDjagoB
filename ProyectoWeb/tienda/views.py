6a0037f651b05bd7eefccc05fc508f4dec08a56b
Juan Ignacio <zalbak87@gmail.com>
2022-06-05 01:44:20 -0300

    Clase Video 49 configuracion admin tienda

--- ProyectoWeb/tienda/views.py
+++ ProyectoWeb/tienda/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .import views
+from .models import Producto
 # Create your views here.
 
 
 def tienda(request):
+
+    productos=Producto.objects.all()
     
-    return render(request,'tienda/tienda.html')
+    return render(request,'tienda/tienda.html',{"productos":productos})
