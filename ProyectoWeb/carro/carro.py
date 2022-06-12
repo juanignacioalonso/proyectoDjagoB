c1905a47480dfdbbb36bfc93fbb9d45348eae68a
Juan Ignacio <zalbak87@gmail.com>
2022-06-12 19:27:52 -0300

    Clase video 53 funciones de del carro

--- ProyectoWeb/carro/carro.py
+++ ProyectoWeb/carro/carro.py
@@ -18,3 +18,32 @@ class Carro:
                 "imagen":producto.imagen.url,
 
             }
+        else:
+            for key,value in self.carro.items():
+                if key==str(producto.id):
+                    value["cantidad"]=value["cantidad"]+1
+                    break #esto es para que pare cuando encuentra el producto sale del bucle y no sigue recorriendo
+        self.guardarCarro()
+
+    def guardarCarro(self):
+        self.session["carro"]=self.carro
+        self.session.modified=True #funcion para modificar
+
+    def eliminar(self,producto): #Para eliminar un producto completo
+        producto.id=str(producto.id)
+        if producto.id in self.carro:
+            del self.carro[producto.id]
+            self.guardarCarro()
+
+    def restarProducto(self,producto):
+        for key,value in self.carro.items():
+                if key==str(producto.id):
+                    value["cantidad"]=value["cantidad"]-1
+                    if value["cantidad"]<1:
+                        self.eliminar(producto)
+                    break 
+        self.guardarCarro()
+
+    def limpiarCarro(self,producto):
+        carro=self.session["carro"]={}
+        self.session.modified=True
