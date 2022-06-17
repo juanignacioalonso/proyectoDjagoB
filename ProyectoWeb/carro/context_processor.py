0597e75a19ced0b45a321e57a7e950396575d199
Juan Ignacio <zalbak87@gmail.com>
2022-06-17 17:55:07 -0300

    Clase Video 57 Mejorando widget

--- ProyectoWeb/carro/context_processor.py
+++ ProyectoWeb/carro/context_processor.py
@@ -6,6 +6,6 @@ def importe_total_carro(request):
     total=0
     miCarro=Carro(request)
     if request.user.is_authenticated:
-        for value in request.session["carro"].items():
+        for key, value in request.session["carro"].items():
             total=total+(float(value["precio"])*value["cantidad"])
     return {"importe_total_carro":total}
--- ProyectoWeb/carro/views.py
+++ ProyectoWeb/carro/views.py
@@ -11,21 +11,21 @@ def agregarProducto(request,producto_id):
     carro=Carro(request)#Creamos el carro
     producto=Producto.objects.get(id=producto_id)#obtenemos el producto
     carro.agregar(producto=producto) #Agregamos el producto al carro
-    return redirect("tienda")
+    return redirect("Tienda")
 
 def eliminarProducto(request,producto_id):
     carro=Carro(request)#Creamos el carro
     producto=Producto.objects.get(id=producto_id)#obtenemos el producto
     carro.eliminar(producto=producto) #Eliminamos el producto al carro
-    return redirect("tienda")
+    return redirect("Tienda")
 
 def restarProducto(request,producto_id):
     carro=Carro(request)#Creamos el carro
     producto=Producto.objects.get(id=producto_id)#obtenemos el producto
     carro.restarProducto(producto=producto) #Restamos el producto al carro
-    return redirect("tienda")
+    return redirect("Tienda")
 
 def LimpiarCarro(request,producto_id):
     carro=Carro(request)#Creamos el carro
     carro.limpiarCarro()
-    return redirect("tienda")
+    return redirect("Tienda")
