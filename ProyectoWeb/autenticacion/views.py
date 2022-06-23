108bc6021439367b73687a3a7238f3393528178c
Juan Ignacio <zalbak87@gmail.com>
2022-06-23 10:58:28 -0300

    Clase video 62 completando en formulario registro

--- ProyectoWeb/autenticacion/views.py
+++ ProyectoWeb/autenticacion/views.py
@@ -1,15 +1,31 @@
+from email import message
 import django
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 # Create your views here.
 
 class VRegistro(View):
-
+    #Muestra el formulario registro
     def get(self,request):
         form= UserCreationForm()
         return render(request,"registro/registro.html",{"form":form})
-
+    #Envio de informacion a la BD
     def post(self,request):
-        pass
+        form= UserCreationForm(request.POST)#Esta instrucción guardara la información en la BD
+        if form.is_valid():
+
+            usuario=form.save()#variable donde guardamos la información del formulario
+            login(request,usuario)#Loguear el usuario
+            return redirect('Home')
+        else:
+            for msg in form.error_messages:
+                messages.error(request,form.error_messages[msg])
+
+            return render(request,"registro/registro.html",{"form":form})
+
+            
+
--- ProyectoWeb/carro/context_processor.py
+++ ProyectoWeb/carro/context_processor.py
@@ -4,8 +4,8 @@ from .carro import Carro
 
 def importe_total_carro(request):
     total=0
-    miCarro=Carro(request)
+    #miCarro=Carro(request)
     #if request.user.is_authenticated:
-    for key, value in request.session["carro"].items():
-        total=total+float(value["precio"])
+    """for key, value in request.session["carro"].items():
+        total=total+float(value["precio"])"""
     return {"importe_total_carro":total}
