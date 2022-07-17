2e1e913b3d55d4ff63355bcee6a3746eabaef26f
Juan Ignacio <zalbak87@gmail.com>
2022-07-17 17:31:09 -0300

    Clase Video 66 corrigiendo el carro

--- ProyectoWeb/ProyectoWebApp/views.py
+++ ProyectoWeb/ProyectoWebApp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, HttpResponse
 
+from carro.carro import Carro
+
 
 
 # Create your views here.
 
 def home(request):
-
+    miCarro=Carro(request) #Esto es para siempre tener iniciado un carro pq si no el context_processor de el carro cuado cerramos la secion y la volvemos a abrir da error pq no tiene un carro iniciado
     return render(request,'ProyectoWebApp/home.html')
 
 
--- ProyectoWeb/carro/context_processor.py
+++ ProyectoWeb/carro/context_processor.py
@@ -4,8 +4,10 @@ from .carro import Carro
 
 def importe_total_carro(request):
     total=0
-    #miCarro=Carro(request)
-    #if request.user.is_authenticated:
-    """for key, value in request.session["carro"].items():
-        total=total+float(value["precio"])"""
+    
+    if request.user.is_authenticated:
+        for key, value in request.session["carro"].items():
+            total=total+float(value["precio"])
+    else:
+        total="Debes hacer login"
     return {"importe_total_carro":total}
