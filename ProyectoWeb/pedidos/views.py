53d1467b27ad5f6826bdc803b56fa9e80056723d
Juan Ignacio <zalbak87@gmail.com>
2022-07-19 15:04:04 -0300

    Clase video 69 Creando app Pedidos

--- ProyectoWeb/pedidos/views.py
+++ ProyectoWeb/pedidos/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from ProyectoWeb.pedidos.models import LineaPedido
+from carro.carro import Carro
+from django.contrib import messages
+
+from pedidos.models import Pedido
 
 # Create your views here.
+@login_required(login_url="/autenticacion/logear")
+def procesar_pedido(request):
+    pedido=Pedido.objects.create(user=request.user)
+    carro=Carro(request)
+    lineas_pedido=list()
+    for key,value in carro.carro.items():
+        lineas_pedido.append(LineaPedido(
+            producto_id=key,
+            cantidad=value("cantidad"),
+            user=request.user,
+            pedido=pedido
+        ))
+
+    LineaPedido.objects.bulk_create(lineas_pedido)#El metodo bulk_create es cosi hiciera muchas instrucciones de inser into procesadas en lotes en este caso los tenemos como en una lista
+    enviar_mail(
+        pedido=pedido,
+        lineas_pedido=lineas_pedido,
+        nombreusuario=request.username,
+        emailusuario=request.usermail
+    )
+    messages.seccess(request,"El pedido se ha creado correctamente")
+    return redirect("../tienda")
+
